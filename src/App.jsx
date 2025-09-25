c2bad2abbca9e209983a446d17d3598abde73517
DinethSheveen <fdineth4000@gmail.com>
2025-09-25 12:08:50 +0100

    Added a default msg to the msg-container

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,24 @@ import ChatMessages from  "./assets/Components/ChatMessages"
 import ChatInput from './assets/Components/ChatInput'
 
 function App() {
-  const [messages,setMessages] = useState([
-    {
-      message : "Hello Chatbot",
-      sender : "user",
-      id:"id1"
-    },
-    {
-      message : "Hello?  How can I help you?",
-      sender : "bot",
-      id:"id2"
-    },
-    {
-      message : "Can you get me today's date",
-      sender : "user",
-      id:"id3"
-    },
-    {
-      message : "Today is September 27",
-      sender : "bot",
-      id:"id4"
-    }
-  ])
-
-  return (
-    <div className='app-container'>
-      <ChatMessages messages = {messages} setMessages={setMessages}/>
-      <ChatInput messages = {messages} setMessages={setMessages}/>
-    </div>
+  const [messages,setMessages] = useState([])
+  
+  
+  return  (
+      <div className='app-container'>
+        {messages.length===0?
+          <>
+            <div className='default-msg'>yours friendly youth chatbot. i could only flip a coin, tell you the date and roll a dice...</div>
+            <ChatMessages messages = {messages} setMessages={setMessages}/>
+            <ChatInput messages = {messages} setMessages={setMessages}/>
+          </>
+        :
+          <>
+            <ChatMessages messages = {messages} setMessages={setMessages}/>
+            <ChatInput messages = {messages} setMessages={setMessages}/>
+          </>
+        }
+    </div> 
   )
 }
 
